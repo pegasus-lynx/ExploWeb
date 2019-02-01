27c81b533f192a193bce662da491097d8695881c
pegasus-lynx <dipeshkrj14@gmail.com>
2019-02-01 16:47:48 +0530

    Initial HomePage Basic

---
+++ src/app/appbar/appbar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-appbar',
+  templateUrl: './appbar.component.html',
+  styleUrls: ['./appbar.component.css']
+})
+export class AppbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/recommender/recommender.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recommender',
+  templateUrl: './recommender.component.html',
+  styleUrls: ['./recommender.component.css']
+})
+export class RecommenderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
