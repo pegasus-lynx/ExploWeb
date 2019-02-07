cb0affdadeb9dbe86bf239fcdab59a9f3aaf1a89
pegasus-lynx <dipeshkrj14@gmail.com>
2019-02-07 19:24:06 +0530

    Removed Previos Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AppbarComponent } from './appbar/appbar.component';
-import { HeroComponent } from './hero/hero.component';
-import { FooterComponent } from './footer/footer.component';
-import { HomeComponent } from './home/home.component';
-import { DetailsComponent } from './details/details.component';
-import { RecommenderComponent } from './recommender/recommender.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    AppbarComponent,
-    HeroComponent,
-    FooterComponent,
-    HomeComponent,
-    DetailsComponent,
-    RecommenderComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
